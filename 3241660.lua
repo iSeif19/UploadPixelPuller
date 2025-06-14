261b77cd623d6dccb07a7354a2092a6df42cdc2d
iSeif19 <seifelsayed1669@gmail.com>
2025-06-14 19:30:53 +0300

    Update manifest and lua for AppID 3241660

---
+++ 3241660.lua
@@ -0,0 +1,7 @@
+-- Copyright © 2025 Pixel Sanctuary Discord Server
+-- All rights reserved.
+-- https://discord.gg/BRwMpTga2J
+
+addappid(3241660)
+addappid(3241661,1,"0a0fcffb2de6fb0fa6caff076a5a8cb960903d17320968d5c042fd5a4f9ad2ad")
+setManifestid(3241661,"6932727267039151271", 0)
