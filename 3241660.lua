b84b65ce5ecb09cd007954653ea54e67b60d7bbd
iSeif19 <seifelsayed1669@gmail.com>
2025-06-15 05:30:21 +0300

    Update 3241660.lua

---
+++ 3241660.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 3241660
+-- Name: R.E.P.O.
+addappid(3241660) -- R.E.P.O.
+addappid(3241661,0,"0a0fcffb2de6fb0fa6caff076a5a8cb960903d17320968d5c042fd5a4f9ad2ad") -- Main Game Content (Windows Content)
+setManifestid(3241661,"6932727267039151271")
