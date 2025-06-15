39a7a32f380804c2ab8e0ba8c67bfdd96a1487e8
iSeif19 <seifelsayed1669@gmail.com>
2025-06-15 05:23:21 +0300

    Update 2215390.lua

---
+++ 2215390.lua
@@ -0,0 +1,10 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 2215390
+-- Name: Five Nights at Freddy's: Secret of the Mimic
+addappid(2215390) -- Five Nights at Freddy's: Secret of the Mimic
+addappid(2215391,0,"d87a5534a048a1a7a81c896212cff7fa105e3104a74914bf1473552d72347211") -- Main Game Content (Windows Content)
+setManifestid(2215391,"3833650524465557678")
+addappid(228989,0,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- Main Game Content (Windows Content)
+setManifestid(228989,"550968249685141759")
+addappid(228990,0,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- Main Game Content (Windows Content)
+setManifestid(228990,"1829726630299308803")
