c6e63f05176ca0765bd69269036c00253e891344
iSeif19 <seifelsayed1669@gmail.com>
2025-06-15 05:33:00 +0300

    Update 3489700.lua

---
+++ 3489700.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 3489700
+-- Name: Stellar Blade™
+addappid(3489700) -- Stellar Blade™
+addappid(3489701,0,"2266ca31f6f2666ebe75906201e72f9398694030ff1c8214aacebcf74dc94ee3") -- Main Game Content (Windows Content)
+setManifestid(3489701,"3315828999867726085")
