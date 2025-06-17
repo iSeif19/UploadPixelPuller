1ce1c528181cbd910e2d578bf2665101d6e30670
iSeif19 <seifelsayed1669@gmail.com>
2025-06-17 21:11:33 +0300

    [bot] Update files for AppID: 2133520

---
+++ 2133520.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 2133520
+-- Name: Ertugrul of Ulukayin
+addappid(2133520) -- Ertugrul of Ulukayin
+addappid(2133521,0,"01a536a66993f6a86afe23e020ffb386621f7b1c33113b83fbebca5943dc0397") -- Main Game Content
+setManifestid(2133521,"1275269269826287828")
