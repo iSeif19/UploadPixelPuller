810d97b6dc4e4d9ceddf929c4f51cea90701c412
iSeif19 <seifelsayed1669@gmail.com>
2025-06-17 18:09:48 +0300

    [bot] Update files for AppID: 2511320

---
+++ 2511320.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 2511320
+-- Name: Tour de France 2025
+addappid(2511320) -- Tour de France 2025
+addappid(2511321,0,"84c2bbcc1b6d67b4578837cd1f0de82a11df5d39dc81c4fb85fdf10a0d532955") -- Main Game Content
+setManifestid(2511321,"2597876683864964027")
