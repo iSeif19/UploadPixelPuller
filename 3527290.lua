f20b8352607b459fa529d278c209d974d6b5286d
iSeif19 <seifelsayed1669@gmail.com>
2025-06-17 17:44:42 +0300

    [bot] Update files for AppID: 3527290

---
+++ 3527290.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 3527290
+-- Name: PEAK
+addappid(3527290) -- PEAK
+addappid(3527291,0,"f55585bb6a00ba332421256f80ba05e27afc38fec6170b9a763138244c506f05") -- Main Game Content
+setManifestid(3527291,"9113505127872420731")
