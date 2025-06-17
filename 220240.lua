dc1998f7f20a6be7ed147c4e6820e04f92cd4dab
iSeif19 <seifelsayed1669@gmail.com>
2025-06-17 17:34:11 +0300

    [bot] Update files for AppID: 220240

---
+++ 220240.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 220240
+-- Name: Far Cry 3
+addappid(220240) -- Far Cry 3
+addappid(220241,0,"3dc1c40f6a253fbda33a9cef065e0df1cb4e16b53ab150559cfc67c95bdbc0aa") -- Main Game Content
+setManifestid(220241,"5896142001500010150")
