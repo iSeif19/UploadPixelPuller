a53fa4f2b462bfca670d3c88f8f4d050107816a9
iSeif19 <seifelsayed1669@gmail.com>
2025-06-18 09:43:01 +0300

    [bot] Update files for AppID: 2953480

---
+++ 2953480.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 2953480
+-- Name: Lunar Eclipse
+addappid(2953480) -- Lunar Eclipse
+addappid(2953481,0,"2a966395acc7d2cc1c60362d668bae6e0b283d86ba2251ae54a6ca3151d39c09") -- Main Game Content
+setManifestid(2953481,"3011614230228233420")
