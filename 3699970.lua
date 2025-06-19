d154fa27a7ce723a1005adfac804a56e290c5935
iSeif19 <seifelsayed1669@gmail.com>
2025-06-19 03:27:48 +0300

    Update 3699970 files

---
+++ 3699970.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 3699970
+-- Name: Blasfemia
+addappid(3699970) -- Blasfemia
+addappid(3699971,0,"bd479351e02e020c397dab0bcfeadd8517a06e4ff272b2a2fed335c47f1d5c75") -- Main Game Content (Windows Content)
+setManifestid(3699971,"6738322212555104952")
