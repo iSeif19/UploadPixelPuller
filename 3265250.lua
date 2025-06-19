cb87c7d5d06a8fc2320604748d4969d38eb1b1e4
iSeif19 <seifelsayed1669@gmail.com>
2025-06-19 04:34:18 +0300

    Update 3265250 files

---
+++ 3265250.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 3265250
+-- Name: MindsEye
+addappid(3265250) -- MindsEye
+addappid(3265251,0,"4314da2fbb6b1038e0f8199b23f2f2c4ab77a7b1fb15289036bdc4fb0178a0a1") -- Main Game Content (Windows Content)
+setManifestid(3265251,"7935051142793396921")
