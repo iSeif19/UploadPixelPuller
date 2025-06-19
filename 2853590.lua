2b8a707e180738728b0b4bf78e5ececa5af72183
iSeif19 <seifelsayed1669@gmail.com>
2025-06-19 22:49:54 +0300

    [bot] Update files for AppID: 2853590

---
+++ 2853590.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 2853590
+-- Name: Void War
+addappid(2853590) -- Void War
+addappid(2853591,0,"d5701bf19577767fbae5a71a1115ee2dc76fc65a52dcb920f924c98432166c46") -- Main Game Content
+setManifestid(2853591,"7123289996475238397")
