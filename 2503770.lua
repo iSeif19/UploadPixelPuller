ac702dcd75bc2924be0a80e8190e076f228e36be
iSeif19 <seifelsayed1669@gmail.com>
2025-06-20 23:57:19 +0300

    [bot] Update files for AppID: 2503770

---
+++ 2503770.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 2503770
+-- Name: House of Legacy
+addappid(2503770) -- House of Legacy
+addappid(2503771,0,"13bc89e933d261de7af8bc6b3535e664a82327d9c80adeadbdc74f485d4c939f") -- Main Game Content (Windows Content)
+setManifestid(2503771,"906947035844000082")
