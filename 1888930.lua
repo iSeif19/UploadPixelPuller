6be54e95b834d71ea3893b1cf46955964f37bd21
iSeif19 <seifelsayed1669@gmail.com>
2025-06-20 23:48:06 +0300

    [bot] Update files for AppID: 1888930

---
+++ 1888930.lua
@@ -0,0 +1,12 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 1888930
+-- Name: The Last of Us™ Part I
+addappid(1888930) -- The Last of Us™ Part I
+addappid(1888931,0,"19cd197b4bc7e62551196ec84d00da135a4dedeb5edca50804001bf9e90b6b70") -- Main Game Content (Windows Content)
+setManifestid(1888931,"3173929499971686238")
+addappid(1888931,0,"19cd197b4bc7e62551196ec84d00da135a4dedeb5edca50804001bf9e90b6b70") -- Main Game Content (Windows Content)
+setManifestid(1888931,"4376931508237195356")
+addappid(1888932,0,"20a675f9d2a0efd0cf0f8985b2a1c67d5d64e1c2b5aee665d3692969b28b1be4") -- Main Game Content (Windows Content)
+setManifestid(1888932,"3853558256762083169")
+addappid(1888932,0,"20a675f9d2a0efd0cf0f8985b2a1c67d5d64e1c2b5aee665d3692969b28b1be4") -- Main Game Content (Windows Content)
+setManifestid(1888932,"7992746905878846452")
