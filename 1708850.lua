2fb7300587c5f8c166536e6add7b3170422343ca
iSeif19 <seifelsayed1669@gmail.com>
2025-06-20 23:59:02 +0300

    [bot] Update files for AppID: 1708850

---
+++ 1708850.lua
@@ -0,0 +1,12 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 1708850
+-- Name: Drug Dealer Simulator 2
+addappid(1708850) -- Drug Dealer Simulator 2
+addappid(228989,0,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- Main Game Content (Windows Content)
+setManifestid(228989,"550968249685141759")
+addappid(3064210,0,"a2662bf7c65a5827264dfb642bb6dec3ab58a1e2025302d82f67797be1ae4c79") -- Main Game Content (Windows Content)
+setManifestid(3064210,"5029667159910191789")
+addappid(228990,0,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- Main Game Content (Windows Content)
+setManifestid(228990,"1829726630299308803")
+addappid(1708851,0,"9da58ce930b5409698dfdd9945f19efdbe1e0b7acead98d61080b5b7058f9890") -- Main Game Content (Windows Content)
+setManifestid(1708851,"7381941023270653545")
