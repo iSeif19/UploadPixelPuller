6019b28ccdb321dcba0765850ece6570ec6c11a6
iSeif19 <seifelsayed1669@gmail.com>
2025-06-21 00:00:57 +0300

    [bot] Update files for AppID: 1196590

---
+++ 1196590.lua
@@ -0,0 +1,12 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 1196590
+-- Name: Resident Evil Village
+addappid(1196590) -- Resident Evil Village
+addappid(1196591,0,"7ccf761ec4f6be4f36416cd1304044deb8b419204f823cae95c9d9f64f94297c") -- Main Game Content (Windows Content)
+setManifestid(1196591,"585232905863960465")
+addappid(1196593,0,"95a6023be95ec2186ae5340ee6b7e84f093106e5944e1c8ca09b884622dc41e6") -- Main Game Content (Windows Content)
+setManifestid(1196593,"8955452086167616663")
+addappid(1456360,0,"ba65fa909516f46db9fd95fb6a0e266c4d244b386d260346272290642726b63e") -- Main Game Content (Windows Content)
+setManifestid(1456360,"3932673717585863523")
+addappid(1731080,0,"caa8356d4e747e982b77b28cfcb1df9095bea681dfae768c0a727a3cd96a9279") -- Main Game Content (Windows Content)
+setManifestid(1731080,"767965597380462013")
