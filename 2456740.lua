beca12eb8d82bc609ad0985f830867701a1d396d
iSeif19 <seifelsayed1669@gmail.com>
2025-06-21 00:01:10 +0300

    [bot] Update files for AppID: 2456740

---
+++ 2456740.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 2456740
+-- Name: inZOI
+addappid(2456740) -- inZOI
+addappid(2456741,0,"e91cba2d1d471bdfb115bee76ac997858f84e6ab0a82af5f17d0054f8a3c9686") -- Main Game Content (Windows Content)
+setManifestid(2456741,"8321490079515316932")
