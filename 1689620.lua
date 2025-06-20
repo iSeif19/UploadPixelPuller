6ff1954fe89dcea8e75cfb13e246f0697eef67bc
iSeif19 <seifelsayed1669@gmail.com>
2025-06-20 23:59:56 +0300

    [bot] Update files for AppID: 1689620

---
+++ 1689620.lua
@@ -0,0 +1,12 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 1689620
+-- Name: BLEACH Rebirth of Souls
+addappid(1689620) -- BLEACH Rebirth of Souls
+addappid(228989,0,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- Main Game Content (Windows Content)
+setManifestid(228989,"550968249685141759")
+addappid(1689621,0,"7ff1d0c9c2aea56e2337ba3c85dbf96f3e436b64f4845764d78eb97ad681181c") -- Main Game Content (Windows Content)
+setManifestid(1689621,"5456125316339713265")
+addappid(228988,0,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- Main Game Content (Windows Content)
+setManifestid(228988,"6645201662696499616")
+addappid(228990,0,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- Main Game Content (Windows Content)
+setManifestid(228990,"1829726630299308803")
