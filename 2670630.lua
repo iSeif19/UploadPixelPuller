1e5c2b1e91d501a722c39c951d7930e7d26a4229
iSeif19 <seifelsayed1669@gmail.com>
2025-06-20 23:58:29 +0300

    [bot] Update files for AppID: 2670630

---
+++ 2670630.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 2670630
+-- Name: Supermarket Simulator
+addappid(2670630) -- Supermarket Simulator
+addappid(2670631,0,"72fc26c211612ffaa7b30cdfea152be9628668260d7f8c375980e0f96e445701") -- Main Game Content (Windows Content)
+setManifestid(2670631,"8201240390525612375")
