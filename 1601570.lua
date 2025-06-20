c1e729b87ab7e865a85c8e6f5dff64167d4353aa
iSeif19 <seifelsayed1669@gmail.com>
2025-06-21 00:00:34 +0300

    [bot] Update files for AppID: 1601570

---
+++ 1601570.lua
@@ -0,0 +1,12 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 1601570
+-- Name: The Alters
+addappid(1601570) -- The Alters
+addappid(228989,0,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- Main Game Content (Windows Content)
+setManifestid(228989,"550968249685141759")
+addappid(228988,0,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- Main Game Content (Windows Content)
+setManifestid(228988,"6645201662696499616")
+addappid(228990,0,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- Main Game Content (Windows Content)
+setManifestid(228990,"1829726630299308803")
+addappid(1601571,0,"0439da537fb9ca417bd1efa7f41890efff942c7cae69a68d72ff61d8b3e2ca8b") -- Main Game Content (Windows Content)
+setManifestid(1601571,"4806372360105956027")
