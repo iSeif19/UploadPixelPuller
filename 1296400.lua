50af54012dfc7e26b1ae869a081889673538637a
iSeif19 <seifelsayed1669@gmail.com>
2025-06-20 23:58:21 +0300

    [bot] Update files for AppID: 1296400

---
+++ 1296400.lua
@@ -0,0 +1,10 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 1296400
+-- Name: Architect Life: A House Design Simulator
+addappid(1296400) -- Architect Life: A House Design Simulator
+addappid(229000,0,"3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") -- Main Game Content (Windows Content)
+setManifestid(229000,"4622705914179893434")
+addappid(228989,0,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- Main Game Content (Windows Content)
+setManifestid(228989,"550968249685141759")
+addappid(228990,0,"44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- Main Game Content (Windows Content)
+setManifestid(228990,"1829726630299308803")
