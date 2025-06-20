1875f3df127fa98ff207d26e56a1f4a11bd9e081
iSeif19 <seifelsayed1669@gmail.com>
2025-06-20 23:59:12 +0300

    [bot] Update files for AppID: 250900

---
+++ 250900.lua
@@ -0,0 +1,6 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 250900
+-- Name: The Binding of Isaac: Rebirth
+addappid(250900) -- The Binding of Isaac: Rebirth
+addappid(250902,0,"ad8a374561fd8769aac243917372237482caa7a94aafbcdaa45adc4208916b7a") -- Main Game Content (Windows Content)
+setManifestid(250902,"4994611894646808503")
