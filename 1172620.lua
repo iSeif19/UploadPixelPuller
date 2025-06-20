b2f13663f8acab2bcd70837bbaea636c47648ee8
iSeif19 <seifelsayed1669@gmail.com>
2025-06-20 23:59:09 +0300

    [bot] Update files for AppID: 1172620

---
+++ 1172620.lua
@@ -0,0 +1,18 @@
+-- Generated Lua Manifest by Pixel Sanctuary
+-- AppID 1172620
+-- Name: Sea of Thieves: 2025 Edition
+addappid(1172620) -- Sea of Thieves: 2025 Edition
+addappid(2309050,0,"e496bd343a45a22ca0148b587298f7701885f32f944ce4217de1c0ba4fb0f786") -- Main Game Content (Windows Content)
+setManifestid(2309050,"5827126359140529534")
+addappid(228989,0,"ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- Main Game Content (Windows Content)
+setManifestid(228989,"550968249685141759")
+addappid(2838632,0,"bf2d7c38224c5b155dce8a0857ecc24110e9afe71c7f4205e51330b8f99373a5") -- Main Game Content (Windows Content)
+setManifestid(2838632,"8106968498466588924")
+addappid(2838631,0,"398266b3450812ecd97e3f428e6546c939c5ebda28899a391cafa3ff2311f2cf") -- Main Game Content (Windows Content)
+setManifestid(2838631,"739321582875573975")
+addappid(2838640,0,"68937ad2c3fcbdd9f020ab2a0bf862931c990fafd559f2fd67a362a76f4569ec") -- Main Game Content (Windows Content)
+setManifestid(2838640,"502403447961499835")
+addappid(228988,0,"1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- Main Game Content (Windows Content)
+setManifestid(228988,"6645201662696499616")
+addappid(1172621,0,"d0a7c81c3c4ec69d5646531c500f6f8e46c7a42a74c6baf68a210c8db2f82117") -- Main Game Content (Windows Content)
+setManifestid(1172621,"752100477012823914")
